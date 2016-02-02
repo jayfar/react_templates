e07ade378ec078ad315f572f02bbbcadb7a317f6
jason <jason.farnen@f-e-t.com>
2016-02-02 11:37:20 -0500

    Switch to using browserify directly and not gulp-browserify. Also added gulp-uglify step in the release build step http://stackoverflow.com/questions/24992980/how-to-uglify-output-with-browserify-in-gulp

--- 03_GulpBrowserifyNodeToStatic/gulpfile.js
+++ 03_GulpBrowserifyNodeToStatic/gulpfile.js
@@ -1,21 +1,49 @@
 var gulp = require('gulp');
-var browserify = require('gulp-browserify');
-var concat = require('gulp-concat');
+var browserify = require('browserify');
+var uglify = require('gulp-uglify');
+// gulp-util is used to track down any errors with uglify: https://github.com/terinjokes/gulp-uglify/issues/76
+var gutil = require('gulp-util');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+
 
 gulp.task('browserify', function() {
-    gulp.src('src/js/app.js')
-      .pipe(browserify({transform:'reactify'}))
-      .pipe(concat('app.js'))
-      .pipe(gulp.dest('dist/js'));
+    return browserify('src/js/app.js', {transform:'reactify', debug: false}) 
+        .bundle()
+        .pipe(source('app.js')) // gives streaming vinyl file object
+        .pipe(buffer()) // <----- convert from streaming to buffered vinyl file object
+        .pipe(uglify().on('error', gutil.log)) // now gulp-uglify works
+        .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('browserify_debug', function() {
+    return browserify('src/js/app.js', {transform:'reactify', debug: true}) // ignore:['react', 'react-dom', 'jQuery', 'react-bootstrap', 'react-router', 'react-router-bootstrap']
+        .bundle()
+        .pipe(source('app.js')) // gives streaming vinyl file object
+        .pipe(buffer()) // <----- convert from streaming to buffered vinyl file object
+        //.pipe(uglify()) // no gulp-uglify in debug
+        .pipe(gulp.dest('dist/js'));
 });
 
 gulp.task('copy', function() {
     gulp.src('src/index.html')
       .pipe(gulp.dest('dist'));
+    
+    gulp.src('src/*.png')
+      .pipe(gulp.dest('dist'));
+
 });
 
-gulp.task('default',['browserify', 'copy']);
+
+gulp.task('apply-prod-environment', function() {
+    process.env.NODE_ENV = 'production';
+});
+
+gulp.task('default',['browserify_debug', 'copy']);
+
+gulp.task('release',['apply-prod-environment', 'browserify', 'copy']);
 
 gulp.task('watch', function() {
-    gulp.watch('src/**/*.*', ['default']);
+    gulp.watch('srcClient/**/*.*', ['default']);
 });
+
